33d18ffb8b6df9cc13a3f6b350da82f6bf90f510
Matt Keef <mkeef1@gmail.com>
2014-12-07 23:59:01 -0600

    note model updated

--- server/models/note.js
+++ server/models/note.js
@@ -2,14 +2,25 @@
 
 'use strict';
 
-var pg = require('../postgres/manager');
+var pg = require('../postgres/manager'),
+    crypto = require('crypto'),
+    bucket = process.env.AWS_BUCKET,
+    AWS    = require('aws-sdk'),
+    s3     = new AWS.s3(),
+    async  = require('async'),
+    fs     = require('fs');
 
 function Note(){
 }
 
-Note.create = function(user, obj, cb){
-  pg.query('select add_note($1, $2, $3, $4)', [user.id, obj.title, obj.body, obj.tags], function(err, results){
-    cb(err, results && results.rows ? results.rows[0].add_note : null);
+Note.create = function(user, obj, images, cb){
+  var awsLinks = {links: ['none']};
+  obj.tags = obj.tags ? formatTags(obj.tags[0]) : 'default';
+  if(images.file){
+    awsLinks = reformatAwsFiles(images.file);
+  }
+  pg.query('select add_note($1, $2, $3, $4, $5)', [user.id, obj.title, obj.body, obj.tags, awsLinks.links], function(err, results){
+    uploadFilesToS3(images.file, awsLinks, cb);
   });
 };
 
@@ -20,10 +31,41 @@ Note.query = function(user, query, cb){
 };
 
 Note.showOne = function(noteId, note, cb){
-  pg.query('select * from notes where notes.id = ' + noteId, [note.id], function(err, results){
+  pg.query('select * from get_notes($1)', [note.id], function(err, results){
     console.log('server note', results);
     cb(err, results && results.rows ? results.rows : null);
   });
 };
 
 module.exports = Note;
+
+function formatTags(tags){
+  tags = tags.split(',').map(function(s){return s.trim();}).join(',');
+  return tags;
+}
+
+function reformatAwsFiles(images){
+  var folder = crypto.randomBytes(48).toString('hex'),
+  links = [];
+  links = images.map(function(f){
+    return 'htt[s://s3.amazonaws.com/' + process.env.AWS_BUCKET + '/' + folder + '/' + f.originalFilename;
+  });
+  return {links: links, folder: folder};
+}
+
+function uploadFilesToS3(images, awsLinks, cb){
+  if(!images){return cb();}
+  var index = 0;
+  async.forEach(images, function(file, callback){
+    if((/^image/).test(file.headers['content-type'])){
+      fs.readFile(file.path, function(err, body){
+        var params = {Bucket: bucket, Key: awsLinks.folder + '/' + file.originalFilename, Body: body, ACL: 'public-read'};
+        index++;
+        s3.putObject(params, function(err){
+          console.log('S3 upload: ', err);
+          callback(null);
+        });
+      });
+    }else{callback(null);}
+  }, cb);
+}
